d984a6c0e8e9204e1f365320eab014f8dc9728a5
mh <mh@immerda.ch>
2017-08-02 15:09:29 +0200

    this fact can still be useful on newer facter releases

--- lib/facter/default_ipaddress.rb
+++ lib/facter/default_ipaddress.rb
@@ -1,31 +1,27 @@
-if Gem::Version.new(Facter::FACTERVERSION) < Gem::Version.new('3.0')
-  require 'facter/util/ip'
-
-  Facter.add("default_interface") do
-    confine :kernel => :linux
-    setcode do
-      def_interface = nil
-      routes = File.read('/proc/net/route')
-      routes.split("\n").each do |line|
-        parts = line.split
-        if parts[1] == "00000000"
-          def_interface = Facter::Util::IP.alphafy(parts[0])
-          break
-        end
+Facter.add("default_interface") do
+  confine :kernel => :linux
+  setcode do
+    def_interface = nil
+    routes = File.read('/proc/net/route')
+    routes.split("\n").each do |line|
+      parts = line.split
+      if parts[1] == "00000000"
+        def_interface = parts[0].gsub(/[^a-z0-9_]/i, '_')
+        break
       end
-      def_interface
     end
+    def_interface
   end
+end
 
-  Facter.add("default_ipaddress") do
-    confine :kernel => :linux
-    setcode do
-      def_ipaddress = nil
-      def_interface = Facter.fact(:default_interface).value
-      if def_interface
-        def_ipaddress = Facter.value("ipaddress_#{def_interface}")
-      end
-      def_ipaddress
+Facter.add("default_ipaddress") do
+  confine :kernel => :linux
+  setcode do
+    def_ipaddress = nil
+    def_interface = Facter.fact(:default_interface).value
+    if def_interface
+      def_ipaddress = Facter.value("ipaddress_#{def_interface}")
     end
+    def_ipaddress
   end
 end
